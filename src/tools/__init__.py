92629fbe3cdcd9415b3cada8b0c2d9d475bb0f1e
Anton <awiklund76@gmail.com>
2024-12-01 19:32:32 +0200

    refactor: Update tools to follow consistent naming convention

--- src/tools/__init__.py
+++ src/tools/__init__.py
@@ -1,20 +1,20 @@
 from .calendar import calendar_tool
-from .coding import code_tool
+from .code import code_tool
 from .database import database_tool
 from .statistical_analysis import statistical_analysis_tool
-from .summarization import summary_tool
-from .task_managment import task_managment_tool
-from .web_search import search_web_tool
+from .summary import summary_tool
+from .task_management import task_management_tool
+from .search_web import search_web_tool
 
 DEFAULT_TOOLS = [
     "summary_tool",
     "search_web_tool",
     "statistical_analysis_tool",
-    "task_managment_tool",
+    "task_management_tool",
     "code_tool",
     "database_tool",
     "calendar_tool"
 ]
 
 __all__ = ["summary_tool", "search_web_tool", "statistical_analysis_tool",
-           "task_managment_tool", "code_tool", "database_tool", "calendar_tool", "DEFAULT_TOOLS"]
+           "task_management_tool", "code_tool", "database_tool", "calendar_tool", "DEFAULT_TOOLS"]
